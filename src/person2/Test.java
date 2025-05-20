f3985d5993917a0dfd3a2ce97136dfe2f6bb1366
yukamaru <yuun.vv07@gmail.com>
2025-05-20 21:46:52 +0900

    コンストラクタ１、２を使ってインスタンス化

--- src/person2/Test.java
+++ src/person2/Test.java
@@ -6,6 +6,16 @@ public class Test {
 		// TODO 自動生成されたメソッド・スタブ
 
 		Person taro = new Person();
+		taro.name = "taro";
+		taro.age = 18;
+		System.out.println(taro.name);
+		System.out.println(taro.age);
+		//コンストラクタ①を使ったインスタンス化
+		
+		Person jiro = new Person("jiro",20);
+		System.out.println(jiro.name);
+		System.out.println(jiro.age);
+		//コンストラクタ②を使ったインスタンス化
 	}
 
 }
